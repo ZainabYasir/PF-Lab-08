7bf324a1348ce4115417405a0ea52a8b259bfa9d
ZainabYasir <zaynb5637@gmail.com>
2024-11-15 01:14:34 +0500

    main.cpp

--- Task 01/main.cpp
+++ Task 01/main.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
 int day;
-cout<<"Enter a day:";
+cout<<"Enter a day(1 for Monday,2 for Tuesday,3 for Wednesday,4 for Thursday,5 for Friday,6 for Saturday,7 for Sunday):";
 cin>>day;
 switch(day){
 case 1:
